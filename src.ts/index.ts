d999fef058272e39d55c0e99918f82c6ec9260c5
vicnaum <vicnaum@gmail.com>
2022-03-16 16:28:27 +0100

    export added

--- src.ts/index.ts
+++ src.ts/index.ts
@@ -1,5 +1,6 @@
 export { BigNumber, BigNumberish } from "./bignumber";
 export { formatFixed, FixedFormat, FixedNumber, parseFixed } from "./fixednumber";
+export { BigDecimal } from "./BigDecimal";
 
 // Internal methods used by address
 export { _base16To36, _base36To16 } from "./bignumber";
